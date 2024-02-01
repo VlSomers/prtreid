cb93c8e9ad181102c0c7c3e9c7917b670e37d98a
Vladimir <somers.vladimir@gmail.com>
2024-02-01 14:04:45 +0100

    Add verbose config

--- torchreid/scripts/main.py
+++ torchreid/scripts/main.py
@@ -229,15 +229,17 @@ def build_torchreid_model_engine(cfg):
     logger = Logger(cfg)
     writer = Writer(cfg)
     set_random_seed(cfg.train.seed)
-    print('Show configuration\n{}\n'.format(cfg))
-    print('Collecting env info ...')
-    print('** System info **\n{}\n'.format(collect_env_info()))
+    if cfg.project.verbose:
+        print('Show configuration\n{}\n'.format(cfg))
+        print('Collecting env info ...')
+        print('** System info **\n{}\n'.format(collect_env_info()))
     if cfg.use_gpu:
         torch.backends.cudnn.benchmark = True
     datamanager = build_datamanager(cfg)
     engine_state = EngineState(cfg.train.start_epoch, cfg.train.max_epoch)
     writer.init_engine_state(engine_state, cfg.model.bpbreid.masks.parts_num)
-    print('Building model: {}'.format(cfg.model.name))
+    if cfg.project.verbose:
+        print('Building model: {}'.format(cfg.model.name))
     model = torchreid.models.build_model(
         name=cfg.model.name,
         num_classes=datamanager.num_train_pids,
@@ -250,7 +252,8 @@ def build_torchreid_model_engine(cfg):
     num_params, flops = compute_model_complexity(
         model, cfg
     )
-    print('Model complexity: params={:,} flops={:,}'.format(num_params, flops))
+    if cfg.project.verbose:
+        print('Model complexity: params={:,} flops={:,}'.format(num_params, flops))
     if cfg.model.load_weights and check_isfile(cfg.model.load_weights):
         load_pretrained_weights(model, cfg.model.load_weights)
     if cfg.use_gpu:
@@ -263,9 +266,8 @@ def build_torchreid_model_engine(cfg):
         cfg.train.start_epoch = resume_from_checkpoint(
             cfg.model.resume, model, optimizer=optimizer, scheduler=scheduler
         )
-    print(
-        'Building {}-engine for {}-reid'.format(cfg.loss.name, cfg.data.type)
-    )
+    if cfg.project.verbose:
+        print('Building {}-engine for {}-reid'.format(cfg.loss.name, cfg.data.type))
     engine = build_engine(cfg, datamanager, model, optimizer, scheduler, writer, engine_state)
     return engine, model
 
